a8a15ba2534f58f24d7f69214cc913f8576cd451
Abdullah-Khalil123 <abdullah.khalil123321@gmail.com>
2024-05-04 23:24:14 +0500

    r

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.get("/api/getOccupentRate", (req, res) => {
 app.get("/api/getMonthlyFlow", (req, res) => {
   const [month, year] = [new Date().getMonth(), new Date().getFullYear()];
 
-  const Query = `select roomID, Date, amount from rents where year(Date) = ${year} and month(Date) = ${3};`;
+  const Query = `select roomID, Day(Date), amount from rents where year(Date) = ${year} and month(Date) = ${3};`;
   connection.query(Query, (error, results) => {
     if (error) {
       res.status(400).send("Error at (getMonthlyFlow)API" + error);
